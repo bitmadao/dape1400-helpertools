919cce38707b19d3d6bc6c85204e410cb64b7c3e
Dag Soervoll <dag@pepmagic.no>
2015-09-02 23:49:54 +0200

    WIP: change CMH to allow for less code in main classes when creating menus

--- src/no/hioa/dape1400/s236758/HelperTools/ConsoleMenuHelper.java
+++ src/no/hioa/dape1400/s236758/HelperTools/ConsoleMenuHelper.java
@@ -1,17 +1,39 @@
 package no.hioa.dape1400.s236758.HelperTools;
 
+import java.util.ArrayList;
+
+
 /**
  * Created by dags on 20.08.15.
  */
 public class ConsoleMenuHelper {
     private int nextMenuID ;
+    private ArrayList<Runnable> methods ;
+    private int methodSize ;
+    private StringBuilder dashboard ;
 
-    public ConsoleMenuHelper() {
+    public ConsoleMenuHelper(String menuName) {
+        this.dashboard = new StringBuilder().append(menuName).append("\n");
+        this.methods = new ArrayList<>() ;
         this.nextMenuID = 1 ;
+    }
+
+    public void addItem(String menuText, Runnable methodName) {
+        this.dashboard.append(menuItemText(menuText)) ;
+        this.methods.add(this.getNextMenuID(), methodName);
+        this.increaseNextMenuID();
     }
 
     public int getNextMenuID() {
         return this.nextMenuID;
+    }
+
+    public int getMethodSize() {
+        this.methodSize = this.methods.size() ;
+        return methodSize ;
+    }
+    public Runnable getMethod(int index) {
+        return methods.get(index) ;
     }
 
     public int getPreviousMenuID() {
@@ -22,9 +44,7 @@ public class ConsoleMenuHelper {
         this.nextMenuID ++;
     }
 
-    public String nextItem(String itemText) {
-        String returnItem = "\t" + this.getNextMenuID() + ") " + itemText + "\n";
-        this.increaseNextMenuID();
-        return returnItem;
+    public String menuItemText(String itemText) {
+        return String.format("\t%d) %s\n",this.getNextMenuID(), itemText);
     }
 }
