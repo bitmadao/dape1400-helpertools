b6735edab6f8413a34ec1311e443fbb09592b71b
Dag Soervoll <dag@pepmagic.no>
2015-09-05 00:14:49 +0200

    various tests

--- src/no/hioa/dape1400/s236758/HelperTools/HelperToolsTestdrive.java
+++ src/no/hioa/dape1400/s236758/HelperTools/HelperToolsTestdrive.java
@@ -6,10 +6,21 @@ package no.hioa.dape1400.s236758.HelperTools;
 public class HelperToolsTestdrive {
     public static void main(String[] args) {
         ConsoleMenuHelper cmh = new ConsoleMenuHelper("Testing 1,2,3") ;
-        cmh.addItem("testing one two three", () ->QuitHelper.enterToContinue());
+        cmh.addItem("testing enterToContinue()", () -> QuitHelper.enterToContinue());
 
         System.out.println("Testing three two one") ;
         cmh.runMenu();
+
+        System.out.print("scanInt(): ");
+        int scanInt = TypeScanner.scanInt() ;
+        System.out.println("scanInt = " + scanInt) ;
+        System.out.print("scanFloat(): ");
+        float scanFloat = TypeScanner.scanFloat();
+        System.out.println("scanFloat = " + scanFloat);
+        System.out.print("scanDouble(): ") ;
+        double scanDouble = TypeScanner.scanDouble() ;
+        System.out.println("scanDouble = " + scanDouble) ;
+
     }
 
     private HelperToolsTestdrive() {
